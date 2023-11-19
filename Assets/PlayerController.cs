e1eeb783ea680a7bf2d782afe88146212f5f9769
artem <fauzzeth@ukr.net>
2023-11-19 10:32:09 +0200

    some unfinished update
    added:
    Terrain + Texture
    player with weird controls
    some nice skybox
    that's all

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class SimplePlayerController : MonoBehaviour
+{
+    [SerializeField] float movementSpeed = 5f;
+    [SerializeField] float shiftSpeed = 10f;
+    [SerializeField] float jumpForce = 7f;
+
+    private bool isGrounded;
+    private float currentSpeed;
+    private Vector3 direction;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        currentSpeed = movementSpeed;
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        direction = new Vector3(moveX, 0, moveZ);
+        direction = transform.TransformDirection(direction);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
+            isGrounded = false;
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            currentSpeed = shiftSpeed;
+        }
+        else
+        {
+            currentSpeed = movementSpeed;
+        }
+
+        // Получаем позицию курсора в мировых координатах
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = transform.position.z;
+
+        // Поворачиваем персонажа в сторону курсора
+        transform.LookAt(mousePosition);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(transform.position + direction * currentSpeed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isGrounded = true;
+    }
+}
